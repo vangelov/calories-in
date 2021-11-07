14a0698a6922282c25b2517fe4427a66beceb051
Vladimir Angelov <vlady.angelov@gmail.com>
2021-11-07 17:05:10 +0200

    Refactor description

--- src/ingredients/PdfIngredientsList/PdfIngredientItem.tsx
+++ src/ingredients/PdfIngredientsList/PdfIngredientItem.tsx
@@ -1,8 +1,7 @@
 import { Text, StyleSheet, View } from '@react-pdf/renderer'
 import { Food, FoodId } from 'foods'
 import { IngredientForm } from 'ingredients'
-import { Portion } from 'portions'
-import { getAmountFromPortionToGrams } from 'portions/useGetAmount'
+import { Portion, getIngredientPortionDescription } from 'portions'
 import { Stats } from 'stats'
 import PdfStat from 'stats/PdfStat'
 import PdfStatsLayout from 'stats/PdfStatsLayout'
@@ -15,50 +14,14 @@ type Props = {
   portionsById: Record<string, Portion>
 }
 
-const UNITS_WITH_DISTANCE = ['oz', 'tsp', 'tbsp', 'fl oz', 'c']
-
-function getTest(
-  portion: Portion,
-  amount: string,
-  portionsById: Record<string, Portion>,
-  food: Food
-) {
-  const { gramsPerAmount, millilitersPerAmount } = portion
-
-  const weightInGrams = Math.round(
-    getAmountFromPortionToGrams(Number(amount), portion.id, food, portionsById)
-  )
-
-  if (gramsPerAmount) {
-    const a = `${amount}${
-      UNITS_WITH_DISTANCE.includes(portion.unit) ? ' ' : ''
-    }${portion.unit}`
-
-    if (gramsPerAmount !== 1) {
-      return `${a} (${weightInGrams}g)`
-    }
-
-    return a
-  }
-
-  if (millilitersPerAmount) {
-    return `${amount}${UNITS_WITH_DISTANCE.includes(portion.unit) ? ' ' : ''}${
-      portion.unit
-    } (${weightInGrams}g)`
-  }
-
-  return ''
-}
-
 function PdfIngredientItem({
   ingredientForm,
   stats,
   foodsById,
   portionsById,
 }: Props) {
-  const { portionId, foodId, amount } = ingredientForm
+  const { foodId } = ingredientForm
   const food = foodsById[foodId]
-  const portion = portionsById[portionId]
 
   return (
     <View style={styles.root}>
@@ -71,7 +34,12 @@ function PdfIngredientItem({
                 { color: getComputedColorFromChakra('gray.600') },
               ]}
             >
-              {getTest(portion, amount, portionsById, food)} {food.name}
+              {getIngredientPortionDescription(
+                ingredientForm,
+                foodsById,
+                portionsById
+              )}{' '}
+              {food.name}
             </Text>
 
             {ingredientForm.notes && (
