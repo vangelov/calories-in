474ddc17f89489b5a96b6f204518aeaa473e0555
Vladimir Angelov <vlady.angelov@gmail.com>
2021-12-21 12:33:42 +0200

    Remove CalendarPlus icon bg color

--- src/icons/CalendarPlus.tsx
+++ src/icons/CalendarPlus.tsx
@@ -3,7 +3,7 @@ import { chakra } from '@chakra-ui/react'
 function CalendarPlus({ size = 24, ...rest }) {
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" fill="none" {...rest}>
-      <rect width="24" height="24" fill="white" />
+      <rect width="24" height="24" />
       <path
         d="M21 13V6C21 4.89543 20.1046 4 19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H12"
         stroke="black"
