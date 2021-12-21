e88b614c6ed3189da1c8645aa6fb6f1b2a7f86ac
Vladimir Angelov <vlady.angelov@gmail.com>
2021-12-21 11:16:14 +0200

    Update add day button icon

--- src/icons/CalendarPlus.tsx
+++ src/icons/CalendarPlus.tsx
@@ -2,57 +2,50 @@ import { chakra } from '@chakra-ui/react'
 
 function CalendarPlus({ size = 24, ...rest }) {
   return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      focusable="false"
-      aria-hidden="true"
-      pointer-events="none"
-      stroke-linejoin="round"
-      stroke-linecap="round"
-      stroke-width="2"
-      stroke="currentColor"
-      fill="none"
-      {...rest}
-    >
-      <g>
-        <rect
-          id="svg_1"
-          ry="2"
-          rx="2"
-          height="18"
-          width="18"
-          y="4"
-          x="2.94816"
-        />
-        <line id="svg_2" y2="6" x2="16" y1="2" x1="16" />
-        <line id="svg_3" y2="6" x2="8" y1="2" x1="8" />
-        <line id="svg_4" y2="10" x2="21" y1="10" x1="3" />
-        <line
-          stroke="#000"
-          stroke-linecap="undefined"
-          stroke-linejoin="undefined"
-          id="svg_6"
-          y2="18.49025"
-          x2="11.97408"
-          y1="13.52387"
-          x1="11.97408"
-          fill="none"
-        />
-        <line
-          transform="rotate(90 11.9741 16.0071)"
-          stroke="#000"
-          stroke-linecap="undefined"
-          stroke-linejoin="undefined"
-          id="svg_10"
-          y2="18.49025"
-          x2="11.97408"
-          y1="13.52387"
-          x1="11.97408"
-          fill="none"
-        />
-      </g>
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" {...rest}>
+      <rect width="24" height="24" fill="white" />
+      <path
+        d="M21 13V6C21 4.89543 20.1046 4 19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H12"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16 2V6"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M17 14V22"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21 18L13 18"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 2V6"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 10H21"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   )
 }
